43647080dad1d5d7b79ec98482e484b1ed5664fe
Tatiana <70781386+TatianaVolkovaa@users.noreply.github.com>
2021-04-08 22:24:53 +0300

    3

--- 2_threaded_server/client.py
+++ 2_threaded_server/client.py
@@ -1,16 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import socket
-from time import sleep
+
+class MyExit(Exception):
+    pass
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+server = input("адрес сервера -> ")
+port = input("порт сервера -> ")
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+if server == '':
+    server = '127.0.0.1'
+if port == '':
+    port = 9090
 
-data = sock.recv(1024)
+try:
+    sock.connect((server, int(port)))
+   
+    print('Server IP-> '+str(server)+' Port-> '+str(port))
+    host = sock.getsockname()
+    print('client IP-> '+str(host[0])+' Port-> '+str(host[1]))
+
+
+except ConnectionRefusedError as c:
+    print(c)
+    print("отказано в соединении")
+    exit()
+
+
+print("введите данные -> ")
+
+while True:
+
+    try:
+        promt=input()
+    except KeyboardInterrupt as k:
+        print(k)
+        print("программа остановлена")
+        exit()
+
+    try:
+        result=sock.send(promt.encode())
+        if not result:
+            raise Exception("нет данных!")
+    except Exception as e:
+        print(e)
+        exit()
+
+    try:
+        data = sock.recv(1024).decode("utf8")
+        if (len(data)==0):
+            raise Exception("нет данных или потеря связи!")
+        if ('exit' or 'sstop') in data.lower():
+            raise MyExit("конец связи!")
+
+    except ConnectionResetError as e:
+        print(e)
+        print("потеряли соединение с сервером")
+        sock.close()
+        exit()
+
+    except Exception as s:
+        print(s)
+        sock.close()
+        exit()
+
+    except MyExit as ex:
+        print(ex)
+        break
+        exit()
+
+    print(data)
 
 sock.close()
 
-print(data.decode())
